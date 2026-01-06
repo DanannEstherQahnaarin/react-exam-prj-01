3692a3e305a48392f5adf0970294e9ddceeb2b84
kmg <deepestdark@gmail.com>
2026-01-06 15:19:34 +0900

    DOC MODIFY

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import "./App.css";
 import * as Gals from "./Gallerys";
 
 export default function App() {
+  let name = "Hedy Lamarr";
+
   return (
     <>
-      <h1>Hedy Lamarr's Todos</h1>
+      <h1>{name}'s Todos</h1>
+      
       <Gals.Profile />
-      <ul>
+      <ul
+        style={{
+          backgroundColor: "black",
+          color: "pink",
+        }}
+      >
         <li>Invent new traffic lights</li>
         <li>Rehearse a movie scene</li>
         <li>Improve the spectrum technology</li>
--- src/Gallerys.tsx
+++ src/Gallerys.tsx
@@ -1,18 +1,34 @@
-export function Profile(){
-    return (
-      <
-        img src='https://i.imgur.com/MK3eW3Am.jpg'
-        alt='KJ'
-      />
-    )
-  }
-  
-  export function Gallery(){
-    return(
-      <section>
-        <Profile/>
-        <Profile/>
-        <Profile/>
-      </section>
-    )
-  }
+import { getImageUrl } from "./Util";
+
+interface Person {
+  name: string;
+  imageId: string;
+}
+
+export function Avatar({ person, size }: { person: Person; size: number }) {
+  return (
+    <img
+      className="avatar"
+      src={getImageUrl(person)}
+      alt={person.name}
+      width={size}
+      height={size}
+    />
+  );
+}
+
+export function Profile() {
+  return (
+    <Avatar person={{ name: "Lin Lanying", imageId: "1bX5QH6" }} size={100} />
+  );
+}
+
+export function Gallery() {
+  return (
+    <section>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
