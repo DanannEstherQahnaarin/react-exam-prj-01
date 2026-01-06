9c57c087a98f2f260241bce67d9072e7afb1b633
kmg <deepestdark@gmail.com>
2026-01-06 14:13:38 +0900

    file add

---
+++ src/Gallerys.tsx
@@ -0,0 +1,18 @@
+export function Profile(){
+    return (
+      <
+        img src='https://i.imgur.com/MK3eW3Am.jpg'
+        alt='KJ'
+      />
+    )
+  }
+  
+  export function Gallery(){
+    return(
+      <section>
+        <Profile/>
+        <Profile/>
+        <Profile/>
+      </section>
+    )
+  }
